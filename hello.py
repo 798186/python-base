6553f9095b403d65168befe129433f626bc84f81
Eduardo Oliveira <edu.oliveira995@gmail.com>
2023-09-12 00:47:29 -0300

    Adicionado algumas variaveis

--- hello.py
+++ hello.py
@@ -1,5 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Hello Word Multi Linguas
 
-#Este programa imprime Hello 
-print("Hello, Colorados, Word!!!")
+Dependendo da Lingua configurada no ambiente o programa exibe a mensagem 
+correspondete.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    exporr LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Eduardo Oliveira"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG","en_US")[:5]
+
+msg = "Hello, Word!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+print(msg)
 
